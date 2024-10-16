ef543c76978ec8ea00ff1e999c63a80f82e00024
Ramanathan <rama.gct@gmail.com>
2024-10-16 23:02:33 +0530

    deleting ec2

--- main.tf
+++ main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = "ap-south-1"  # Change to your preferred region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-04a37924ffe27da53"  # Change to your preferred AMI
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform-ec2-example"
-  }
-}
-
 
 resource "aws_instance" "ec2usinggithubactions" {
   ami           = "ami-04a37924ffe27da53"  # Change to your preferred AMI
