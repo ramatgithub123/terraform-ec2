1b846f6c4dab950ebcfac3703b6d386bcc77ba94
Ramanathan <rama.gct@gmail.com>
2024-10-20 23:16:00 +0530

    minikube

--- main.tf
+++ main.tf
@@ -11,9 +11,14 @@ resource "aws_instance" "Terraform-ec2-example" {
     Name = "Terraform-ec2-example"
   }
 
-}
 
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
+user_data = <<-EOF
+            #!/bin/bash
+            sudo yum update -y
+            sudo yum install -y docker
+            sudo systemctl start docker
+            sudo systemctl enable docker
+            curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+            sudo install minikube-linux-amd64 /usr/local/bin/minikube
+            EOF
 }
