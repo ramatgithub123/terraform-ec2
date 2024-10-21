ed427dbf2a25b3fa229b3c81051c23757979fc19
Ramanathan <rama.gct@gmail.com>
2024-10-22 01:08:05 +0530

    install docker minikube

--- main.tf
+++ main.tf
@@ -11,15 +11,22 @@ resource "aws_instance" "Terraform-ec2-example" {
     Name = "Terraform-ec2-example"
   }
 
-
-user_data = <<-EOF
-            #!/bin/bash
-            sudo yum update -y
-            sudo yum install -y docker
-            sudo systemctl start docker
-            sudo systemctl enable docker
-            curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-            sudo install minikube-linux-amd64 /usr/local/bin/minikube
-            EOF
-
+provisioner "remote-exec" {
+  inline = [
+    "sudo yum update -y",
+    "sudo yum install -y docker",
+    "sudo service docker start",
+    "sudo usermod -aG docker ec2-user",
+    "curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+    "chmod +x minikube",
+    "sudo mv minikube /usr/local/bin/",
+    "minikube start --driver=docker"
+  ]
+ connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("C:/Users/913486/learn/key/ec2")
+    host        = "${aws_instance.Terraform-ec2-example.public_ip}"
+  }
+}
 }
