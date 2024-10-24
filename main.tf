04d1af7afb5082cde8fc12175b01754155c73e54
ramatgithub123 <40395107+ramatgithub123@users.noreply.github.com>
2024-10-25 00:57:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,22 +11,22 @@ resource "aws_instance" "Terraform-ec2-example" {
     Name = "Terraform-ec2-example"
   }
 
-provisioner "remote-exec" {
-  inline = [
-    "sudo yum update -y",
-    "sudo yum install -y docker",
-    "sudo service docker start",
-    "sudo usermod -aG docker ec2-user",
-    "curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
-    "chmod +x minikube",
-    "sudo mv minikube /usr/local/bin/",
-    "minikube start --driver=docker"
-  ]
- connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("ec2_key.pem")
-    host        = "${aws_instance.Terraform-ec2-example.public_ip}"
-  }
-}
+#provisioner "remote-exec" {
+# inline = [
+#    "sudo yum update -y",
+#    "sudo yum install -y docker",
+#    "sudo service docker start",
+#    "sudo usermod -aG docker ec2-user",
+#    "curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+#    "chmod +x minikube",
+#    "sudo mv minikube /usr/local/bin/",
+#    "minikube start --driver=docker"
+#  ]
+# connection {
+#    type        = "ssh"
+#    user        = "ec2-user"
+#    private_key = file("ec2_key.pem")
+#    host        = "${aws_instance.Terraform-ec2-example.public_ip}"
+#  }
+#}
 }
